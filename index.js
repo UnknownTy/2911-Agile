6c185faa9ff8c91f054df6006800e6e197c9ea71
Unknown <ty@gimpel.ca>
2021-05-03 11:09:31 -0700

    Added in basic bot functionality. Just use to add future features to this.

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const ejsLayouts = require("express-ejs-layouts");
-const session = require("express-session")
+const session = require("express-session");
+const Discord = require("discord.js");
+const axios = require("axios")
 require("dotenv").config()
+const client = new Discord.Client();
 const PORT = process.env.PORT || 5050
+const botToken = process.env.botToken
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
@@ -28,6 +32,11 @@ app.set("view engine", "ejs");
 //Routes can go here
 
 
+//Bot responses go here
+client.on("ready", () => {
+  console.log("Bot is ready and logged in!")
+})
+
 
 
 app.listen(PORT, function () {
@@ -35,3 +44,5 @@ app.listen(PORT, function () {
       `Server running! Available on port ${PORT} 🚀`
     );
   });
+
+client.login(botToken)
