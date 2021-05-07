3c54b0f25e3364b801f6c16b3d9ca7979a199dd1
bradleygee8 <bcgee8@gmail.com>
2021-05-06 23:59:50 -0700

    Co-authored-by: Unknown <UnknownTy@users.noreply.github.com> Co-authored-by: judy-chen <judy-chen@users.noreply.github.com>

--- commands.js
+++ commands.js
@@ -16,23 +16,23 @@ const defaultEmbed = (ctx)=> {
 
 const loadResponse = ((data, ctx) => {
     let embed = defaultEmbed(ctx)
-    for(let stat of Object.entries(data)){
+    for (let stat of Object.entries(data)) {
         let name = properNames[stat[0]]
-        //Check to see if the name is stored in our list of acceptable data
-        if (name){
-            if (name == properNames.updated){
+            //Check to see if the name is stored in our list of acceptable data
+        if (name) {
+            if (name == properNames.updated) {
                 //Converts the unix timestamp to a proper format
                 let last_update = new Date(stat[1])
-                //Formats the timestamp to look nice
+                    //Formats the timestamp to look nice
 
                 //Before: 2021-05-03T19:30:437Z
-                var numFormatted = last_update.toISOString().replace(/T/,' ').replace(/\..+/,'')
-                //After: 2021-05-03 19:30:43
-            } else{
+                var numFormatted = last_update.toISOString().replace(/T/, ' ').replace(/\..+/, '')
+                    //After: 2021-05-03 19:30:43
+            } else {
                 var numFormatted = new Intl.NumberFormat("en-CA").format(stat[1])
             }
             //Add the data to the embed
-            embed.addField(name, numFormatted, inline=true)
+            embed.addField(name, numFormatted, inline = true)
         }
     }
     return embed
@@ -74,12 +74,12 @@ module.exports = {
     statAll: ctx => {
         //Gets data from public covid API
         axios.get(`https://corona.lmao.ninja/v2/all`)
-        .then(response => {
-            let statInfo = loadResponse(response.data, ctx)
-            .setThumbnail("https://eoimages.gsfc.nasa.gov/images/imagerecords/8000/8108/ipcc_bluemarble_west_front.jpg")
-            //Return the data to the discord channel that requested it
-            ctx.channel.send(embed=statInfo)
-    })
+            .then(response => {
+                let statInfo = loadResponse(response.data, ctx)
+                    .setThumbnail("https://eoimages.gsfc.nasa.gov/images/imagerecords/8000/8108/ipcc_bluemarble_west_front.jpg")
+                    //Return the data to the discord channel that requested it
+                ctx.channel.send(embed = statInfo)
+            })
     },
 
     statCountry: (ctx, country) => {
@@ -106,7 +106,7 @@ module.exports = {
             case (age < 18):
                 msg.channel.send(message + "between June and July.")
                 break;
-            case (age < 35): 
+            case (age < 35):
                 msg.channel.send(message + "in June.");
                 break;
             case (age < 45):
@@ -123,7 +123,7 @@ module.exports = {
                 break;
             case (age <= 150):
                 msg.channel.send("If you are 80 and over and living or assessed for living in long-term care facilities or assisted living, " +
-                "you may get your 1st dose between December 2020 to February 2021. Otherwise, you may get your first dose between February to April.")
+                    "you may get your 1st dose between December 2020 to February 2021. Otherwise, you may get your first dose between February to April.")
                 break;
             case (age > 150):
                 msg.channel.send(`Are you sure you're ${age} years-old?`) 
@@ -139,8 +139,8 @@ module.exports = {
             .setImage(url="https://www2.gov.bc.ca/assets/gov/covid-19/immunization/immunization-plan-phase-3.png")
             .setColor(0xa7ff78) 
             .setTimestamp()
-            .setDescription("You may be eligible to get your vaccine sooner, regardless of age, if you are part of an exception or priority group. Some examples include those who are clinically vulnerable,"+ 
-            "those who work in volunerable settings, and those who are indigenous. Please check the following link to determine whether you are part of an exception group.")
+            .setDescription("You may be eligible to get your vaccine sooner, regardless of age, if you are part of an exception or priority group. Some examples include those who are clinically vulnerable," +
+                "those who work in volunerable settings, and those who are indigenous. Please check the following link to determine whether you are part of an exception group.")
             .addField("Link to BC's Immunization Plan and Exceptions:", "https://www2.gov.bc.ca/gov/content/covid-19/vaccine/plan#phases");
         msg.channel.send(exceptionEmbed);
     },
