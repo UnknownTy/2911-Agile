7ec5650f7d4f7e5df316d63ad7149379bfcb10c8
Unknown <ty@gimpel.ca>
2021-05-07 00:24:21 -0700

    Version 0.2 (#20)
    * Updated formatting for better legibility
    
    * Added dev message to branch. This will appear on all bots run on the dev branch.
    
    * Changed formatting for message checking to use switch cases. Added base of "help" command.
    
    * Added new prefix command.
    
    * Created a new section for constants to clear some space in the Commands file. This is for clarity. Updated the help command to show all valid commands
    
    * Added testing to the dev dependencies. This includes Jest and Supertest
    
    * Updated help command to give additional information on each command. Added comments throughout for better documentation.
    
    * Fix boundry cases for when command (#17)
    
    * Restriction command (#18)
    
    * Fix boundry cases for when command
    
    * Add restriction command
    
    Co-authored-by: Unknown <ty@gimpel.ca>
    
    * Update .gitignore
    
    Added .vscode
    
    * Pull request for Unit tests (#19)
    
    * Co-authored-by: Unknown <UnknownTy@users.noreply.github.com>
    Co-authored-by: judy-chen <judy-chen@users.noreply.github.com>
    
    * Delete launch.json
    
    We don't need .vscode files.
    
    * Update package.json
    
    Moved large test dependencies to Dev dependencies. Regular users don't need this :)
    
    * Update index.js
    
    Updated Index.js to have it work with the testing.
    
    Co-authored-by: bradleygee8 <bcgee8@gmail.com>
    
    Co-authored-by: judy-chen <shiyichen95@gmail.com>
    Co-authored-by: bradleygee8 <bcgee8@gmail.com>

--- index.js
+++ index.js
@@ -14,61 +14,89 @@ const botToken = process.env.botToken
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(ejsLayouts);
-app.use(session(
-  {
+app.use(session({
     secret: "secret",
     resave: false,
     saveUninitialized: false,
     cookie: {
-      httpOnly: true,
-      secure: false,
-      maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: false,
+        maxAge: 24 * 60 * 60 * 1000,
     },
-  }
-))
+}))
 
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
-let prefix = '!'
-//Routes can go here
+const messageHandler = msg => {
+    //Check if the message starts with the prefix, or is sent by the bot
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+    //Remove all excess spaces and splice out the args
+    const args = msg.content.slice(prefix.length).trim().split(/ +/);
+    //Grab the command out of the list of arguments
+    const command = args.shift().toLowerCase();
+    msg.channel.send("DEV BRANCH -- REMOVE THIS BEFORE MAIN")
+    switch(true){
+      //Used to change the prefix
+      case (command === "prefix"):
+        if (!args[0]){
+          cmd.argsUsage(msg, "prefix", prefix)
+        }
+        else if(args[0].length > 1){
+          msg.channel.send(`Invalid new prefix ${args[0]}, cannot be longer than one character`)
+        } else {
+          prefix = args[0] 
+          msg.channel.send(`Prefix updated to \`${prefix}\``)
+        }
+        break;
+      //Used to get help on the commands
+      case (command === "help"):
+          cmd.help(msg, prefix, args)
+        break;
+      //Used to show covid statistics
+      case (command === "stat"):
+        if (args.length == 0){
+          cmd.statAll(msg)
+        } else {
+          cmd.statCountry(msg, args[0].toLowerCase())
+        }
+        break;
+      //Used to show when a user can get vaccinated
+      case (command === "when"):
+        if (args.length === 0 || args.length > 1) {
+          cmd.argsUsage(msg, "when", prefix)  
+        }
+        else if (args[0] === "exception") {
+          cmd.vaccineException(msg)
+        }
+        else {
+          cmd.vaccineWhen(msg, args[0])
+        }
+        break;
+      }
+  }
+
+let prefix = '!' // Default prefix. Adjust this to read from some settings later.
+    //Routes can go here
 
 
+if(require.main === module){
 //Bot responses go here
 client.on("ready", () => {
   console.log("Bot is ready and logged in!")
 })
 
-client.on("message", msg => {
-  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
-  const args = msg.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
-  if (command === "stat"){
-    if (args.length == 0){
-      cmd.statAll(msg)
-    } else {
-      cmd.statCountry(msg, args[0].toLowerCase())
-    }
-  }
-  else if (command === "when") {
-    if (args.length === 0 || args.length > 1) {
-      cmd.argsUsage(msg, "when")  
-    }
-    else if (args[0] === "exception") {
-      cmd.vaccineException(msg)
-    }
-    else {
-      cmd.vaccineWhen(msg, args[0])
-    }
-  }
-  
-})
+client.on("message", messageHandler)
 
-
+//Hosts the express server
 app.listen(PORT, function () {
     console.log(
       `Server running! Available on port ${PORT} 🚀`
     );
   });
 
+//Logs the bot into Discord
 client.login(botToken)
+} else {
+    module.exports = { messageHandler }
+}
